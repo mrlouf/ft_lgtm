2a1c43b03781ace86c7a37d087eb9a228a954a88
nicolas@desktop <ponchon.nicolas@gmail.com>
2026-07-21 22:10:56 +0200

    cd: add app annotation to write update the tag of the images for Argo

--- frontend/src/components/RunButton.tsx
+++ frontend/src/components/RunButton.tsx
@@ -1,6 +1,29 @@
 export default function RunButton() {
     function handleRun() {
         console.log("Running code...");
+
+        try {
+
+            const res = fetch("/api/run", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    code: "console.log('Hello, World!');",
+                    language: "javascript",
+                }),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log("Run result:", data);
+                })
+                .catch((error) => {
+                    console.error("Error running code:", error);
+                });
+        } catch (error) {
+            console.error("Error running code:", error);
+        }
     }
 
     return (
