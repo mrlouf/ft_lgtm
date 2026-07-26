fafd6ea977cb7110ac1b4e94f9d66ff3354789e7
Nicolas (laptop) <ponchon.nicolas@gmail.com>
2026-07-26 15:57:17 +0200

    successfully publish source code and output as IPFS blocks via the Kubo daemon

--- backend/internal/ipfs/publisher.go
+++ backend/internal/ipfs/publisher.go
@@ -1,6 +1,7 @@
 package ipfs
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"log"
@@ -9,6 +10,11 @@ import (
 	"github.com/ipfs/kubo/client/rpc"
 	iface "github.com/ipfs/kubo/core/coreiface"
 )
+
+type ResponseCID struct {
+	Source string `json:"source"`
+	Stdout string `json:"stdout"`
+}
 
 type IPFSPublisher struct {
 	node iface.CoreAPI
@@ -22,12 +28,32 @@ func NewIPFSPublisher(daemonURL string) (*IPFSPublisher, error) {
 	return &IPFSPublisher{node: node}, nil
 }
 
-func (ipfs *IPFSPublisher) Publish(ctx context.Context, data []byte) (string, error) {
+func (i *IPFSPublisher) Publish(ctx context.Context, source []byte, stdout []byte) (ResponseCID, error) {
 
 	log.Println("publish: start")
 
+	response := ResponseCID{}
+
+	sourceReader := bytes.NewReader(source)
+	sourceBlock, err := i.node.Block().Put(ctx, sourceReader)
+	if err != nil {
+		return response, fmt.Errorf("ipfs add source: %w", err)
+	}
+
+	outputReader := bytes.NewReader(stdout)
+	outputBlock, err := i.node.Block().Put(ctx, outputReader)
+	if err != nil {
+		return response, fmt.Errorf("ipfs add stdout: %w", err)
+	}
+
+	response.Source = sourceBlock.Path().String()
+	response.Stdout = outputBlock.Path().String()
+
+	log.Println("publish: block published:", response.Source)
+	log.Println("publish: block published:", response.Stdout)
+
 	log.Println("publish: done")
 
-	return "", nil
+	return response, nil
 
 }
