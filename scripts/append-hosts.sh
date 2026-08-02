a0ac02694e1f467e9d60d1f66d38a3431a3ee41a
nicolas@desktop <ponchon.nicolas@gmail.com>
2026-08-02 17:18:01 +0200

    build: add a script to append hosts to /etc/hosts

---
+++ scripts/append-hosts.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Colours
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Colour
+
+CLUSTER_NAME=$1
+
+set -e
+
+HOSTS='172.18.0.2	lgtm.local argocd.lgtm.local ipfs.lgtm.local'
+
+if [[ $(grep -c "$HOSTS" /etc/hosts) -gt 0 ]]; then
+    echo -e "${YELLOW}Hosts already exist in /etc/hosts${NC}"
+    exit 0
+
+else
+    echo -e "${BLUE}Adding hosts to /etc/hosts${NC}"
+    sudo -- sh -c "echo '$HOSTS' >> /etc/hosts"
+    echo -e "${GREEN}/etc/hosts configured${NC}"
+
+fi
+
